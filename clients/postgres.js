3520914db67473b58257aef76ba01e79cd87c669
Enrique Goudet <goudetenrique@gmail.com>
2021-11-05 22:26:01 -0600

    group trackIds to greatly reduce api calls

--- clients/postgres.js
+++ clients/postgres.js
@@ -3,6 +3,6 @@ const config = require('../const');
 const postgres = require('knex')({
     client: 'pg',
     connection: 'postgres://localhost/playlistgenerator'
-  });
+});
 
 module.exports = postgres;
--- endpoints/tracks/getTrackAudioFeatures.js
+++ endpoints/tracks/getTrackAudioFeatures.js
@@ -7,6 +7,8 @@ const postgres = require('../../clients/postgres');
 const { showResults } = require('../../middleware/showResults');
 
 const SPOTIFY_URL = 'https://api.spotify.com/v1/audio-features';
+const MAX_CHARACTERS_FOR_IDS_IN_QUERY = 1500;
+const TRACK_ID_LENGTH = 22;
 
 module.exports = async (req, res, next) => {
 
@@ -15,10 +17,26 @@ module.exports = async (req, res, next) => {
     try {
         const savedTracks = await postgres('track').where({user_id: userId});
 
+        const totalTracks = savedTracks.length;
+        const totalIdsPerGrouping = Math.ceil(MAX_CHARACTERS_FOR_IDS_IN_QUERY / TRACK_ID_LENGTH); // 69
+        const totalGroupings = Math.ceil(totalTracks / totalIdsPerGrouping); // 13
+
+        let idGroupings = [];
+
+        // create idGroupings
+        for (let i = 0; i < totalGroupings; i++) {
+            let currentGrouping = [];
+            for (let j = 0; j < totalIdsPerGrouping; j++)  {
+                currentGrouping.push(savedTracks[(i*totalGroupings)+j].spotify_id); // figure out indexing issue here, almost there
+            }
+            idGroupings.push(currentGrouping.join(','));
+        }
+
+        // greatly decrease number of calls, getting a 429
         const audioFeaturesPromises = await Promise.all(
-            savedTracks.map((track) => {
+            idGroupings.map((grouping) => {
                 return axios({
-                    url: `${SPOTIFY_URL}/${track.spotify_id}`,
+                    url: `${SPOTIFY_URL}/${grouping}`,
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json',
@@ -40,7 +58,8 @@ module.exports = async (req, res, next) => {
         req.info = {
             savedTracks,
             audioFeatures,
-            trackUpdates
+            trackUpdates,
+            idGroupings
         }
         
         return showResults(req, res);
